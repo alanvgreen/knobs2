6bcc080da8a6f1cf32f69f75802504e5d5b35915
Alan Green <alan.green@gmail.com>
2024-05-09 19:44:34 +1000

    Mount local directory to run dev code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Knobs
 
 A many-knobbed Micropython MIDI Controller project for the RP2040.
+
+
+## Directory structure
+
+- picofs - Local copy of filesystem that will end up on the RP2040
---
+++ scripts/dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+THIS_DIR="$(dirname "$0")"
+WORKING_ROOT="$(git -C ${THIS_DIR} rev-parse --show-toplevel)"
+
+(
+    cd ${WORKING_ROOT}/picofs
+    mpremote mount . "$@"
+)
