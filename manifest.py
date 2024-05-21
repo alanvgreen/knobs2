e785670eff69739a3b381a838b7cf0d4c169b3c4
Alan Green <alan.green@gmail.com>
2024-05-22 07:14:44 +1000

    Include MIDI and an example

--- manifest.py
+++ manifest.py
@@ -1,5 +1,8 @@
 # Include all the regular mpy stuff
 include("$(PORT_DIR)/boards/manifest.py")
 
-# Grab the entire content of this directory
+# Ensure USB MIDI is available
+require("usb-device-midi")
+
+# Grab the entire content of the freeze directory
 freeze("freeze")
