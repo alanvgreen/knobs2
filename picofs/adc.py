9dc9d7b355ec6b67a0876bda85b92bc2bb2d5bbb
Alan Green <alan.green@gmail.com>
2024-08-17 15:19:13 +1000

    Scan 18 knobs

--- picofs/adc.py
+++ picofs/adc.py
@@ -1,21 +1,34 @@
 import asyncio
-import machine
+from machine import ADC, Pin
 
 
 class PotReader:
     def __init__(self, pot_holder):
         self._pot_holder = pot_holder
         self._adcs = [
-            machine.ADC(26),
-            machine.ADC(27),
-            machine.ADC(28),
+            ADC(26),
+            ADC(27),
+            ADC(28),
+        ]
+        self._inhibit = Pin(16, Pin.OUT, value=1)
+        self._sel = [
+            Pin(20, Pin.OUT, value=0),
+            Pin(19, Pin.OUT, value=0),
+            Pin(17, Pin.OUT, value=0),
+            Pin(18, Pin.OUT, value=0),
         ]
 
     async def read_one(self, idx):
         """Reads the value of a single pot, 0-17"""
         if idx < 16:
-            # adc 0 and multiplexer
-            return self._adcs[0].read_u16()
+            self._sel[0].value(idx & 1)
+            self._sel[1].value(idx & 2)
+            self._sel[2].value(idx & 4)
+            self._sel[3].value(idx & 8)
+            self._inhibit.value(0)
+            val = self._adcs[0].read_u16()
+            self._inhibit.value(1)
+            return val
         elif idx < 18:
             return self._adcs[idx - 15].read_u16()
         else:
