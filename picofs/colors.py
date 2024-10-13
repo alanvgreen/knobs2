3ed56a4b5ae536a6cbd15deab07b5df66abc8bbf
Alan Green <alan.green@gmail.com>
2024-10-13 19:43:04 +1100

    Brighten up the colors

--- picofs/colors.py
+++ picofs/colors.py
@@ -2,9 +2,9 @@
 # Color format is LSB, red, green, blue
 
 RED    = 0b_0100
-GREEN  = 0b_0010
+GREEN  = 0b_1010
 BLUE   = 0b_0001
 
-YELLOW = 0b_0110
+YELLOW = 0b_1110
 BLACK  = 0b_0000
 WHITE  = 0b_1111
